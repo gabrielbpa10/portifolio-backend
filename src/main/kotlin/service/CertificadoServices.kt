118bd73a5fbf48ac2a219ad357b99f38a7436ca3
Gabriel Braga Paiva Araujo <gabrielaraujoprofissionalti@gmail.com>
2025-08-24 22:35:04 -0300

    Criado endpoint para download de arquivo no firebase

--- src/main/kotlin/service/CertificadoServices.kt
+++ src/main/kotlin/service/CertificadoServices.kt
@@ -1,9 +1,20 @@
 package org.portifolio.service
 
+import com.google.firebase.FirebaseApp
+import io.quarkus.hibernate.orm.panache.PanacheRepository
 import jakarta.enterprise.context.ApplicationScoped
 import org.portifolio.repository.CertificadoRepository
 import org.portifolio.repository.dto.PaginatedResponse
 import org.portifolio.repository.model.Certificado
+import java.util.Optional
+import com.google.firebase.cloud.StorageClient
+import jakarta.ws.rs.GET
+import jakarta.ws.rs.Path
+import jakarta.ws.rs.PathParam
+import jakarta.ws.rs.Produces
+import jakarta.ws.rs.core.Response
+import org.jboss.resteasy.reactive.RestResponse
+import java.io.ByteArrayInputStream
 
 @ApplicationScoped
 class CertificadoServices(
@@ -14,5 +25,17 @@ class CertificadoServices(
         val resultado = certificadoRepository.getListagemPaginado(pagina, tamanho)
         val novosItens = resultado.itens.map { it.copy(url = "https://${it.url}") }
         return resultado.copy(itens = novosItens)
+    }
+
+    fun getDownload(id: Long): ByteArrayInputStream? {
+        val certificadoMetadados = certificadoRepository.findByIdOptional(id)
+            .orElseThrow { RuntimeException("Certificado não encontrado.") }
+        val nome = "certificados/" + id + "/" + certificadoMetadados.titulo
+        val bucket = StorageClient.getInstance().bucket()
+        val blob = bucket.get(nome)
+
+        return blob?.let {
+            ByteArrayInputStream(it.getContent())
+        }
     }
 }
